742b99b858b2d44ce7e58e4bd3456520f4fa9e42
n22012 <n22012@std.it-college.ac.jp>
2023-02-15 05:12:52 +0000

    chap9

--- components/hero.js
+++ components/hero.js
@@ -4,7 +4,7 @@ import cube from 'images/cube.jpg'
 
 export default function Hero ({ title, subtitle, imageOn = false }) {
   return (
-    <div className={styles.Container}>
+    <div className={styles.flexContainer}>
       <div className={styles.text}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
